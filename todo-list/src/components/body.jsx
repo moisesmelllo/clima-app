caed0593c08bd5b6743e74bc6fae46e11a59852f
Moises Melo Viana Ferreira <moisesmvferreira@gmail.com>
2023-08-05 11:01:51 -0300

    feat: axios api and background img

--- todo-list/src/components/body.jsx
+++ todo-list/src/components/body.jsx
@@ -3,13 +3,21 @@ import '../estilos/body.css'
 import axios from 'axios'
 
 const body = () => {
-  const [data, setData] = useState([])
+  const [data, setData] = useState(null)
+  const [temperature, setTemperature] = useState(null)
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('https://atlas.microsoft.com/weather/currentConditions/json?api-version=1.0&query=47.60357,-122.32945&subscription-key={Your-Azure-Maps-Subscription-key}')
+        const response = await axios.get(
+          'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/-23.46603220342741%2C%20-46.42326288965758?unitGroup=metric&key=D2ZTJHGFWFRY3HY9SM83UAAC5&contentType=json'
+        )
+        
+        setData(response.data)
+        const temp = response.data.days[0].temp
+        setTemperature(temp)
       } catch (error) {
-        console.log('Erro ao acessar a API', error);
+        console.log('Erro ao acessar a API', error)
       }
     }
 
@@ -17,12 +25,32 @@ const body = () => {
   }, [])
 
   return (
-    <div className="box">
-      {data.map(item => {
-        <p key={item}>{item}</p>
-      })}
+    <div className={`box ${getBackgroundImageClass()}`}>
+      {data ? (
+        <>
+          <h1 className="temp">{data.days[0].temp}º</h1>
+          <p className="sub-temp">
+            {data.days[0].tempmax}º / {data.days[0].tempmin}º
+          </p>
+          <p className="temp-description">{data.description}</p>
+        </>
+      ) : (
+        <p>Carregando...</p>
+      )}
     </div>
   )
+
+  function getBackgroundImageClass() {
+    if (temperature !== null) {
+      if (temperature < 10) {
+        return 'frio'
+      } else {
+        return 'sol'
+      }
+    }
+
+    return ''
   }
+}
 
 export default body
